e6a0a7212b3aa1d02f28f35cf128cb2e97e6044b
dvaluev91 <dvaluev91@yandex.ru>
2024-04-24 20:18:21 +0300

    Добавьте файлы проекта.

---
+++ Modeule_6.Unit_2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Modeule_6.Unit_2
+{
+    class Human
+    {
+        //Поля класса
+        public string name;
+        public int age;
+
+        //Метод класса
+        public void Greetings()
+        {
+            Console.WriteLine($"Меня зовут {name}, мне {age}");
+        }
+    }
+    struct Animal
+    {
+        //Поля структуры
+        public string type;
+        public string name;
+        public int age;
+
+        //Метод структуры
+        public void Info()
+        {
+            Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
